d410c174844913349f7153838a03667269f9512d
brendonwells <Veritas1>
2018-01-12 11:15:41 +0000

    added html5shiv for backward compatibility

--- _template/metahead.php
+++ _template/metahead.php
@@ -3,4 +3,7 @@
 <head>
     <title>Bootstrap 3 Enhanced Template</title>
     <link rel="stylesheet" href="/_styles/styles.css"/>
+    <!--[if lt IE 9]>
+    <script src="/_scripts/html5shiv.min.js"></script>
+    <![endif]-->
 </head>
