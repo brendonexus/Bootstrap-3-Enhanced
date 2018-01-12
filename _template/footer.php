ad599feeacc6497792645e45226c1a398137590d
brendonwells <Veritas1>
2018-01-12 11:11:05 +0000

    Added wireframe code to metahead and footer

---
+++ _template/footer.php
@@ -0,0 +1,7 @@
+<footer>
+
+</footer>
+<script src="../bower_components/jquery/dist/jquery.min.js"></script>
+<script src="../bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js"></script>
+<script src="../_scripts/scripts.js"></script>
+</html>
---
+++ _template/metahead.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bootstrap 3 Enhanced Template</title>
+    <link rel="stylesheet" href="../_styles/styles.css"/>
+</head>
