e21e68256de4de6c7f6f47ccd989a77974e65145
brendonwells <Veritas1>
2018-01-12 11:12:28 +0000

    Amended wireframe code to use absolute path

--- _template/metahead.php
+++ _template/metahead.php
@@ -2,5 +2,5 @@
 <html>
 <head>
     <title>Bootstrap 3 Enhanced Template</title>
-    <link rel="stylesheet" href="../_styles/styles.css"/>
+    <link rel="stylesheet" href="/_styles/styles.css"/>
 </head>
